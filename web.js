a5898cab0fe28c612bb6b988caee32dda760570d
Tim Costermans <t.costermans+github@gmail.com>
2013-07-09 19:38:47 +0000

    web.js read from index.html

--- web.js
+++ web.js
@@ -4,14 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var content;
-fs.readFileSync('./index.html', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    content = data;
-
-    console.log(content); 
+var content=fs.readFileSync('./index.xhtml','utf8');
+console.log(content); 
 });
 
 
