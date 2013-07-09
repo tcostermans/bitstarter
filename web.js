9154ec9bd09e94a446401bd4d3bba4ec25b29c31
Tim Costermans <t.costermans+github@gmail.com>
2013-07-09 19:53:09 +0000

    script error

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var content=fs.readFileSync('./index.xhtml','utf8');
+var content=fs.readFileSync('./index.html','utf8');
 console.log(content); 
 });
 
