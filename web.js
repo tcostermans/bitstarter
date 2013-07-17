1287200d19e9a2fee48d5bb3334f0ebb393db168
Tim Costermans <t.costermans@gmail.com>
2013-07-17 19:55:16 +0000

    HW4 part 1 step 0

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ var content=fs.readFileSync('./index.html','utf8');
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
